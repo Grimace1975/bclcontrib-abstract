d1b75f5947ee3b20f754a71f1d575be7dbaed501
Sky Morey <Sky Morey@localhost>
2012-03-21 16:43:43 -0500

    Update

--- Packnship.cmd
+++ Packnship.cmd
@@ -8,7 +8,7 @@ if not exist %OUTPUTDIR% mkdir %OUTPUTDIR%
 set BUILD=-Prop Configuration=Release
 
 :: Services
-for /r src %%x in (*.nuspec) do (
+for /r src\ %%x in (*.nuspec) do (
    set v=%%x
    set v=!v:.nuspec=.csproj!
    set v=!v:BclEx-Abstract.csproj=System.Abstract.csproj!
@@ -18,9 +18,8 @@ for /r src %%x in (*.nuspec) do (
    nuget pack "!v!" -OutputDirectory %OUTPUTDIR% %BUILD% )
    ::pause )
 
-
 echo pushing packages
-::for %%x in (%OUTPUTDIR%*.nupkg) do nuget push "%%x" -source http://nuget.degdarwin.com
+for %%x in (%OUTPUTDIR%*.nupkg) do nuget push "%%x" -source http://nuget.degdarwin.com
 
 endlocal
 choice /N /D Y /T 5
